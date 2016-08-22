41e1419a24c386f7a01c0010269a22445b94aa94
Niel Roos <niel.r@superbalist.com>
2016-08-22 09:43:14 +0200

    styling and new markup for main pages

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,45 +1,115 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Laravel Blog</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <!-- Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+		<!-- Default Bootstrap Navbar -->
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Laravel Blog</a>
+				</div>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="/">Home</a></li>
+						<li><a href="/about">About</a></li>
+						<li><a href="/contact">Contact</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account<span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li role="separator" class="divider"></li>
+								<li><a href="#">Separated link</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="jumbotron">
+						<h1>Welcom to My Blog!</h1>
+						<p class="lead">Thanks so much for visiting my blog! It is built with Laravel! Please read my Popular Post.</p>
+						<p><a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a></p>
+					</div>
+				</div>
+			</div> <!-- end of header .row -->
+			<div class="row">
+				<div class="post">
+					<div class="col-md-8">
+						<div class="post">
+							<h3>Post Title</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor euismod quam, ut vehicula felis semper vel...</p>
+							<a href="#" class="btn btn-primary">Read More</a>
+						</div>
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+						<hr>
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
+						<div class="post">
+							<h3>Post Title</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor euismod quam, ut vehicula felis semper vel...</p>
+							<a href="#" class="btn btn-primary">Read More</a>
+						</div>
+
+						<hr>
+
+						<div class="post">
+							<h3>Post Title</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor euismod quam, ut vehicula felis semper vel...</p>
+							<a href="#" class="btn btn-primary">Read More</a>
+						</div>
+
+						<hr>
+
+						<div class="post">
+							<h3>Post Title</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor euismod quam, ut vehicula felis semper vel...</p>
+							<a href="#" class="btn btn-primary">Read More</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-md-offset-1">
+					<h2>Sidebar</h2>
+				</div>
+			</div>
+		</div> <!-- end of .container -->
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
 </html>
