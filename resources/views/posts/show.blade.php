e77f389c73485594ae12b09d5a5faa79b5ba68df
Niel Roos <nielroos@Niels-MacBook-Pro.local>
2016-10-10 22:11:38 +0200

    blog page that lists posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,7 +14,7 @@
 			<div class="well">
 				<dl class="dl-horizontal">
 					<label>Url Slug:</label><br>
-					<p><a href="{{ url($post->slug) }}">{{ url($post->slug) }}</a></p>
+					<p><a href="{{ route('blog.single', $post->slug) }}">{{ route('blog.single', $post->slug) }}</a></p>
 				</dl>
 
 				<dl class="dl-horizontal">
