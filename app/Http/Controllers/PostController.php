fab73a05328d58b1294b691562feedfd3fad253f
Niel Roos <nielroos@Niels-MacBook-Pro.local>
2016-10-12 08:07:07 +0200

    slug bug fixed for updates

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -91,12 +91,21 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
+		$post = Post::find($id);
+
 		// validate the data
-		$this->validate($request, array(
-			'title' => 'required|max:255',
-			'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
-			'body' => 'required'	
-		));
+		if ($request->input('slug') == $post->slug) {
+			$this->validate($request, array(
+				'title' => 'required|max:255',
+				'body' => 'required'	
+			));
+		} else {
+			$this->validate($request, array(
+				'title' => 'required|max:255',
+				'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
+				'body' => 'required'	
+			));
+		}
 
 		$post = Post::find($id);
 
